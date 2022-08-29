829a0ce385b39ffc43c75bce771313e88a8f9cf6
karlissonJR <karlissonj@gmail.com>
2022-08-29 15:27:07 -0300

    add class: Position

---
+++ src/application/App.java
@@ -0,0 +1,10 @@
+package application;
+
+import boardgame.Position;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Position position1 = new Position(1, 3);
+        System.out.println(position1);
+    }
+}
